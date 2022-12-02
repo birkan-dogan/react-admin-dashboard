bdcc6ed8e50180e03b3c0734c931d354500fcd1c
birkan <b.561birkan@gmail.com>
2022-12-03 00:00:08 +0300

    changing package version to avoid use not completed version

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/user.png`}
+                  src={`https://cdn-icons-png.flaticon.com/512/560/560216.png`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
