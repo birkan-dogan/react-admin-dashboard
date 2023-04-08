32aa50c3d36e88d57c2debc275e27487465de5a0
birkan <b.561birkan@gmail.com>
2023-04-08 16:17:27 +0300

    working on Dashboard page 👷‍♂️

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,11 +1,29 @@
-import { Box } from "@mui/system";
+import { Box, Button, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 import Header from "../../components/Header";
+import { DownloadOutlined } from "@mui/icons-material";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+      <Box>
+        <Button
+          sx={{
+            backgroundColor: colors.blueAccent[700],
+            color: colors.grey[100],
+            fontSize: "14px",
+            fontWeight: "bold",
+            padding: "10px 20px",
+          }}
+        >
+          <DownloadOutlined sx={{ mr: "10px" }} /> Download Reports
+        </Button>
       </Box>
     </Box>
   );
