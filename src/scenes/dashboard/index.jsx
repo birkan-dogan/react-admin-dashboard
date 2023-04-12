9ed0341bd96fb7fb3c993e4d34295ce1cb58ed22
birkan <b.561birkan@gmail.com>
2023-04-12 18:03:08 +0300

    Rotate the phone 📱

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -5,13 +5,36 @@ import { DownloadOutlined } from "@mui/icons-material";
 import Row1 from "../../components/rows/Row1";
 import Row2 from "../../components/rows/Row2";
 import Row3 from "../../components/rows/Row3";
+import { useEffect, useState } from "react";
+import RedoIcon from "@mui/icons-material/Redo";
 
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [tilt, setTilt] = useState(false);
+
+  let w = window.innerWidth;
+
+  useEffect(() => {
+    if (w < 480) {
+      setTilt(true);
+      setTimeout(() => {
+        setTilt(false);
+      }, 4000);
+    }
+  }, []);
 
   return (
     <Box>
+      {tilt && (
+        <div className="overlay">
+          <div className="phone">
+            <RedoIcon className="tilt-phone" />
+          </div>
+          <h1>Tilt the Phone to get better experience</h1>
+        </div>
+      )}
+
       <Box
         display="flex"
         justifyContent="space-between"
